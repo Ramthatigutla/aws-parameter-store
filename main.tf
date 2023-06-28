4d0854e9a443723bd8b1d4e004d84ba766e957f0
ram <ramthatigutla@gmail.com>
2023-06-28 14:37:53 +0530

    Roboshop aws parampeter store

--- main.tf
+++ main.tf
@@ -7,8 +7,11 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name = "demo1", value = "demo1", type="String" },
-    { name = "demo2", value = "demo2", type="String" }
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.trrdops.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.trrdops.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.trrdops.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.trrdops.store:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.trrdops.store:8080/", type = "String" },
   ]
 }
 
