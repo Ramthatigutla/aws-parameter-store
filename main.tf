f1264996461eab9959d90bf27b07f9714e649fb9
ram <ramthatigutla@gmail.com>
2023-07-02 18:56:23 +0530

    roboshop compo

--- main.tf
+++ main.tf
@@ -21,21 +21,24 @@ variable "params" {
     { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://mongodb-dev.trrdops.store:27017/catalogue", type = "String" },
 
 
-    { name = "roboshop.dev.payment.cart_host", value = "cart-dev.rdevopsb73.online", type = "String" },
+    { name = "roboshop.dev.payment.cart_host", value = "cart-dev.trrdops.store", type = "String" },
     { name = "roboshop.dev.payment.cart_port", value = "8080", type = "String" },
     { name = "roboshop.dev.payment.user_host", value = "user-dev.trrdops.store", type = "String" },
     { name = "roboshop.dev.payment.user_port", value = "8080", type = "String" },
-    { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.rdevopsb73.online", type = "String" },
+    { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.trrdops.store", type = "String" },
     { name = "roboshop.dev.payment.amqp_user", value = "roboshop", type = "String" },
 
 
-    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.rdevopsb73.online:8080", type = "String" },
-    { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.rdevopsb73.online", type = "String" },
+
+    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.trrdops.store:8080", type = "String" },
+    { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.trrdops.store", type = "String" },
 
 
     { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
-    { name = "roboshop.dev.user.redis_host", value = "redis-dev.rdevopsb73.online", type = "String" },
-    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-{{env}}.rdevopsb73.online:27017/users", type = "String" },
+    { name = "roboshop.dev.user.redis_host", value = "redis-dev.trrdops.store", type = "String" },
+    { name = "roboshop.dev.user.mongo_url", value = "mongodb://mongodb-dev.trrdops.store:27017/users", type = "String" },
+    #passwords will be never part of git repos
+    { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
   ]
 }
 
