cbcdd6316aeeb1fdd6094465e4c64820ec089c6f
ram <ramthatigutla@gmail.com>
2023-07-03 18:37:55 +0530

    makefile

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "aws_ssm_parameter" "params" {
   type = var.params[count.index].type
   value = var.params[count.index].value
 }
-
 variable "params" {
   default = [
     { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.trrdops.store:8080/", type = "String" },
@@ -12,9 +11,11 @@ variable "params" {
     { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.trrdops.store:8080/", type = "String" },
     { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.trrdops.store:8080/", type = "String" },
     { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.trrdops.store:8080/", type = "String" },
+
     { name = "roboshop.dev.cart.redis_host", value = "redis-dev.trrdops.store", type = "String" },
     { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.trrdops.store", type = "String" },
     { name = "roboshop.dev.cart.catalogue_port", value = "8080", type = "String" },
+
     { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://mongodb-dev.trrdops.store:27017/catalogue", type = "String" },
 
